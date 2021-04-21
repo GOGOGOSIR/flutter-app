c61152c2d3afcfdcefd19d819ae89003aa8446db
wanjianqiang <1239531712@qq.com>
2021-04-21 20:58:57 +0800

    feat: demo

---
+++ lib/demo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo111',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('title1'),
+        ),
+        body: Center(
+          child: Text('Hello World1'),
+        ),
+      ),
     );
   }
 }
@@ -109,5 +108,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class _RandomWords extends StatefulWidget {
+  @override
+  __RandomWordsState createState() => __RandomWordsState();
+}
+
+class __RandomWordsState extends State<_RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
