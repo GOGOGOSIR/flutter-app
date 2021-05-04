fa514d07c5e4eb44ad455416c4e0c55d927053f9
wanjianqiang <17688728066@163.com>
2021-05-04 22:48:01 +0800

    feat: list-view demo

--- lib/list-view.dart
+++ lib/list-view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  debugPaintSizeEnabled = true;
   runApp(MyApp());
 }
 
